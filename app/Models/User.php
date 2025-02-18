a61f64979241790bb81a5a2b34b9499daba99b1b
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-18 15:22:23 +0100

    cambios en modelo user add Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable implements FilamentUser
     ];
 
     //Comprueba si el usuario de producción puede acceder al panelde Fialemnte
-    public function canAccessPanel(Panel $panel): bool
+    public function canAccessFilament(Panel $panel): bool
     {
         //return str_ends_with($this->email, '@evidenze.com') && $this->hasVerifiedEmail();
         return true;
