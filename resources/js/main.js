981af0c5fcd98fd1b74077fbdc61ce6182291f09
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-20 22:25:15 +0100

    cambios para que funcione main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,30 @@
+
 console.log('✅ main.js cargado');
+// Importa los plugins en el orden correcto
+import './plugins/waypoints.min.js';
+import './plugins/meanmenu.min.js';
+import './plugins/counterup.js';
+import './plugins/jarallax.min.js';
+import './plugins/nice-select.min.js';
+import './plugins/parallax-scroll.js';
+import './plugins/parallax.min.js';
+import './plugins/slick.min.js';
+import './plugins/swiper.min.js';
+//import './plugins/wow.js';
+import WOW from 'wow.js';
+window.WOW = WOW;
+
+// Importa los scripts vendor que no son módulos ES (y que dependen de jQuery)
+import './vendor/tween-max.js';
+import './vendor/magnific-popup.min.js';
+import './vendor/isotope.pkgd.min.js';
+import './vendor/imagesloaded.pkgd.min.js';
+import './vendor/vanilla.tilt.js';
+import './vendor/jquery-ui.min.js';
+import './vendor/hover-reveal.js';
+import './vendor/ajax-form.js';
+import './vendor/js_circle-progress.min.js';
+
 (function ($) {
   "use strict";
 
@@ -14,7 +40,6 @@ console.log('✅ main.js cargado');
   /*======================================
   Mobile Menu Js
   ========================================*/
-  console.log(typeof $.fn.meanmenu);
   if (typeof $.fn.meanmenu === 'function') {
     $("#mobile-menu").meanmenu({
       meanMenuContainer: ".mobile-menu",
