498e019b5343ba0a39637bbc9e4591c653f34785
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-21 11:54:55 +0100

    modificando vite.condig.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            // input: ['resources/scss/app.scss', 'resources/js/app.js'],
-            input: [
-                'resources/js/app.js', 
-                'resources/scss/app.scss',
-                'resources/js/bootstrap.js',
-                'resources/js/main.js',
-            ],
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
