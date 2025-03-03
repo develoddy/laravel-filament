16a3fc33a1c91fbdda92d3d8d8c863b36d7e2ffd
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-03-03 12:37:06 +0100

    confgurando envio ccorreo

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -29,7 +29,7 @@ class ContactMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Contact Mail',
+            subject: 'Correo electrónico de contacto',
         );
     }
 
